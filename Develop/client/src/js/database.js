e131d228ba8106d9e19c55df8e5bc93de1f5be60
Haillander <haillanderviana@gmail.com>
2023-04-05 18:40:36 +1000

    code added to database

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -12,7 +12,7 @@ const initdb = async () =>
     },
   });
 
-// Adding logic to a method that accepts some content and adds it to the database
+// Added logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) =>{
   console.log('Post to the database');
   const jateDb = await openDB('jate',1);
